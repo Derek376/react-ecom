4e56a37e6b5f313d5cac8398d0736e28aff8164e
Derek <derek46534@gmail.com>
2026-06-01 23:05:50 +0100

    address selection: set up the layout

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import { Step, Stepper, StepLabel } from "@mui/material";
 import React, { useState } from "react";
+import AddressInfo from "./AddressInfo";
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -14,6 +15,11 @@ const Checkout = () => {
           </Step>
         ))}
       </Stepper>
+
+      <div className="mt-5">
+        {activeStep === 0 && <AddressInfo />}
+
+      </div>
     </div>
   );
 };
