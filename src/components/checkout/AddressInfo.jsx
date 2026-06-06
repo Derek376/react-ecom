75bc6f3ac1e7e987540f2d837ccd5cb31a6447ca
Derek <derek46534@gmail.com>
2026-06-06 03:37:36 +0100

    address selection: save selected user address for checkout

--- src/components/checkout/AddressInfo.jsx
+++ src/components/checkout/AddressInfo.jsx
@@ -3,16 +3,18 @@ import Skeleton from "../shared/Skeleton";
 import { useState } from "react";
 import AddressInfoModal from "./AddressInfoModal";
 import AddAddressForm from "./AddAddressForm";
+import { useSelector } from "react-redux";
+import AddressList from "./AddressList";
 
-const AddressInfo = () => {
+const AddressInfo = ({ address }) => {
   const [openAddressModal, setOpenAddressModal] = useState(false);
   const [selectedAddress, setSelectedAddress] = useState("");
   const addNewAddressHandler = () => {
     setSelectedAddress("");
     setOpenAddressModal(true);
   };
-  const noAddressExist = true;
-  const isLoading = false;
+  const noAddressExist = !address || address.length === 0;
+  const { isLoading, btnLoader } = useSelector((state) => state.errors);
   return (
     <div className="pt-4">
       {noAddressExist ? (
@@ -41,9 +43,26 @@ const AddressInfo = () => {
               <Skeleton />
             </div>
           ) : (
-            <div className="space-y-5 pt-6">
-              <p>Address List</p>
-            </div>
+            <>
+              <div className="space-y-5 pt-6">
+                <AddressList
+                  addresses={address}
+                  setSelectedAddress={setSelectedAddress}
+                  setOpenAddressModal={setOpenAddressModal}
+                />
+              </div>
+
+              {address.length > 0 && (
+                <div className="mt-4">
+                  <button
+                    className="px-4 py-2 bg-blue-600 text-white font-medium rounded-sm hover:bg-blue-700 transition-all cursor-pointer"
+                    onClick={addNewAddressHandler}
+                  >
+                    Add New Address
+                  </button>
+                </div>
+              )}
+            </>
           )}
         </div>
       )}
