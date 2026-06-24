a67f1277d3835ad1f0eedba24ddea3d72b52a731
Derek <derek46534@gmail.com>
2026-06-24 20:49:34 +0100

    add layout to display error messages

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserAddresses } from "../../store/actions";
 import toast from "react-hot-toast";
 import Skeleton from "../shared/Skeleton";
+import ErrorPage from "../shared/ErrorPage";
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -92,6 +93,8 @@ const Checkout = () => {
           </button>
         )}
       </div>
+
+      {errorMessage && <ErrorPage message={errorMessage} />}
     </div>
   );
 };
